97c32cc318233f172e51d5f6c7dace0025b023d6
MassCreation <dennisb.bishop@gmail.com>
2015-06-08 00:33:21 -0700

    refactor contoller code

--- app/controllers/api/exercise_sets_controller.rb
+++ app/controllers/api/exercise_sets_controller.rb
@@ -1,14 +1,31 @@
 class Api::ExerciseSetsController < ApplicationController
 
   def index
+  	respond_with :api, exercise_sets
   end
 
   def create
+  	respond_with :api, exercise_set.create(exercise_params)
   end
 
   def update
   end
 
   def destroy
+  	respond_with :api, exercise_set.destroy
   end
+
+  private
+
+  	def exercise_sets
+    	@exercises ||= Exercise.all
+  	end
+
+	def exercise_set
+	    @exercise_set ||= exercise_sets.find(params[:id])
+	end
+
+	def exercise_set_params
+		params.require(:exercise_set).permit(:athlete_id, :training_session_id, :exercise_id, :completed)
+	end
 end
--- app/controllers/api/training_sessions_controller.rb
+++ app/controllers/api/training_sessions_controller.rb
@@ -2,12 +2,15 @@ class Api::TrainingSessionsController < ApplicationController
   before_filter :authenticate_user!, only: [:create, :index, :show]
 
   def index
+    respond_with :api, training_sessions
   end
 
   def show
+    respond_with :api, training_session
   end
 
   def create
+    respond_with :api, TrainingSession.create(training_session_params)
   end
 
   def update
@@ -15,4 +18,18 @@ class Api::TrainingSessionsController < ApplicationController
 
   def destroy
   end
+
+  private 
+
+    def training_sessions
+      @training_sessions ||= Widget.all
+    end
+
+    def widget
+      @widget ||= widgets.find(params[:id])
+    end
+
+    def training_session_params
+      params.permit(:scheduled_on, :complete, :volume, :category_id, :athlete_id, :period_id)
+    end
 end
