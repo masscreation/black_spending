1d8a6f1afe291bee04406b08f069689d2a5edaf4
MassCreation <dennisb.bishop@gmail.com>
2015-06-08 17:55:59 -0700

    add training session api

--- app/controllers/api/training_sessions_controller.rb
+++ app/controllers/api/training_sessions_controller.rb
@@ -17,19 +17,21 @@ class Api::TrainingSessionsController < ApplicationController
   end
 
   def destroy
+    respond_with :api, training_session.destroy
   end
 
   private 
 
     def training_sessions
-      @training_sessions ||= Widget.all
+        @training_sessions ||= TrainingSession.all
     end
 
-    def widget
-      @widget ||= widgets.find(params[:id])
+    def training_session
+        @training_session ||= training_sessions.find(params[:id])
     end
 
     def training_session_params
-      params.permit(:scheduled_on, :complete, :volume, :category_id, :athlete_id, :period_id)
+      params.permit(:scheduled_on, :complete, :volume, :category_id, :athlete_id, :period_id, 
+        exercise_set_attributes: [])
     end
 end
